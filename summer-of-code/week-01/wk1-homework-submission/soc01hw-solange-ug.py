2b3a20d038fd1d787a467dbc14cddc6db52c24ea
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-19 14:42:20 +0200

    Add homework function signatures and documentation

---
+++ summer-of-code/week-01/wk1-homework-submission/soc01hw-solange-ug.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+
+def hours_in_year(year):
+    """
+    Return the number of hours in given year
+    :param year: input year parameter
+    :return: the number of hours in input year
+    """
+
+
+def is_leap_year(year):
+    """
+    Determine whether the input year is leap or not
+    :param year: input year parameter
+    :return: true if the input year is leap, false otherwise
+    """
+
+
+def minutes_in_decade():
+    """
+    Return the number of minutes in a decade
+    :return: minutes in 10 years
+    """
+
+
+def age_in_seconds(birthdate):
+    """
+    Return age in seconds from an input birth date
+    :param birthdate: input date parameter
+    :return: age from the provided birth date in seconds
+    """
+
+
+def processor_timeout(processor):
+    """
+    Return the number of days a given processor (32-bit or 64-bit)
+    will take to time out if it has an integer overflow bug
+    :param processor: 32-bit or 64-bit machine
+    :return: number of days it takes to time out
+    """
+
+
+def fullname_greeting():
+    """
+    Return a greeting with a person's fullname
+    :return: fullname greeting
+    """
+
+
+def bigger_better_number():
+    """
+    Suggest a bigger, better favorite number to the user
+    :return: user's favorite number + 1
+    """
+
+
+def angry_boss_program():
+    """
+    Return an angry boss response to an employee demand
+    :return: angry boss response
+    """
+
+
+def table_of_contents_formatter():
+    """
+    Display a formatted table of contents using center, ljust and rjust functions
+    :return: a formatted table of contents
+    """
