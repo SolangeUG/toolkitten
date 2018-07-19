8b4067559ba7808ceb96dec7ec6707153b7f4200
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-19 12:36:08 +0200

    Working with variables and arrays refactoring

--- summer-of-code/week-01/vars.py
+++ summer-of-code/week-01/vars.py
@@ -1,66 +1,86 @@
-# print('...you can say that again...')
-# print('...you can say that again...')
-
-# a = '...you can say that again...'
-# print(a)
-# print(a)
-
-# name = 'Darlyze Calixte'
-# print('My name is ' + name + '.')
-# print('Wow ' + name)
-# print( name + ' is a really beautiful name!')
-
-# composer = 'Mozart'
-# print(composer)
-
-# composer = 'Tchaikovsky'
-# print('But I prefer ' + composer + ', personally.')
-
-# username = 'Tikwiza' + '-vip'
-# print(username)
-
-# score = 5 * (1+8)
-# print(score)
-# print('score')
-
-old = 8
-new = old
-print(old)
-print(new)
-
-print('')
-
-old = 'eight cats'
-print(old)
-print(new)
+#!/usr/bin/env python3
 
 
-world01 = 'oooooxxoooo'
-world02 = 'oooooxooooo'
-world03 = 'oooooxxoooo'
-world04 = 'oooooxooooo'
-world05 = 'oooxxxooooo'
-world06 = 'oooxoxxoooo'
-world07 = 'ooooxxooooo'
-world08 = 'oooooxxoooo'
-world09 = 'oooooxooooo'
-world10 = 'oooooxooooo'
-world11 = 'oooooxxoooo'
-
-world = 'oooooxxoooooooooxxoooooooooxxoooooooooxxoooooooooxxoooooooooxxoooooooooxxoooooooooxxoooooooooxxoooooooooxxoooooooooxxoooo'
-
-world = [ 'o', 'x', 'o' ]
-
-world = [[ 'o', 'x', 'o' ],
-         [ 'o', 'x', 'o' ],
-         [ 'o', 'x', 'o' ]
-         ]
-
-world[1][1]
+def using_variables():
+    print("**** Using variables ****")
+    a = '...you can say that again...'
+    print(a)
+    name = 'Darlyze Calixte'
+    print('My name is ' + name + '.')
+    print('Wow ' + name)
+    print(name + ' is a really beautiful name!')
 
 
+def reusing_variables():
+    print("**** Reusing variables ****")
+    composer = 'Mozart'
+    print(composer)
+    composer = 'Tchaikovsky'
+    print('But I prefer ' + composer + ', personally.')
 
 
+def composing_variables():
+    print("**** Composing variables ****")
+    username = 'Tikwiza' + '-vip'
+    print(username)
+    score = 5 * (1 + 8)
+    print(score)
 
 
+def substituting_variables():
+    print("**** Substituting variables ****")
+    old = 8
+    new = old
+    print(old)
+    print(new)
+    print('')
+    old = 'eight cats'
+    print(old)
+    print(new)
 
+
+def using_arrays():
+    print("**** Using arrays ****")
+    world01 = 'oooooxxoooo'
+    world02 = 'oooooxooooo'
+    world03 = 'oooooxxoooo'
+    world04 = 'oooooxooooo'
+    world05 = 'oooxxxooooo'
+    world06 = 'oooxoxxoooo'
+    world07 = 'ooooxxooooo'
+    world08 = 'oooooxxoooo'
+    world09 = 'oooooxooooo'
+    world10 = 'oooooxooooo'
+    world11 = 'oooooxxoooo'
+    world = world01 + world02 + world03 + world04 \
+        + world05 + world06 + world07 + world08 \
+        + world09 + world10 + world11
+    print(world)
+
+    # 1-dimensional array
+    world = ['o', 'x', 'o']
+    print(world[0])
+    print(world[2])
+
+    # an array of arrays
+    world = [['o', 'x', 'o'],
+             ['o', 'x', 'o'],
+             ['o', 'x', 'o']]
+    print(world[1][1])
+
+
+# TESTS
+print()
+using_variables()
+
+print()
+reusing_variables()
+
+print()
+composing_variables()
+
+print()
+substituting_variables()
+
+print()
+using_arrays()
